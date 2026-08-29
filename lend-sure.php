050064da32577fa5f0b0dd9038ab662f75e71716
Moses Cursor Ssebunya <ssebunya.moses2@gmail.com>
2026-08-29 12:25:29 +0300

    Bump Your Loan Ledger to v1.4.0 and load data-management modules

--- lend-sure.php
+++ lend-sure.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Your Loan Ledger
  * Description: Lightweight personal loan tracking for WordPress with 20% monthly interest, partial payments, extensions, penalties, transaction history, and signed loan acknowledgement records.
- * Version: 1.3.1
+ * Version: 1.4.0
  * Plugin URI: https://github.com/Successfulsebunya/lend-sure
  * Author: Moses Cursor
  * Author URI: https://mosescursor.com
@@ -16,7 +16,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-define( 'LENDSURE_VERSION', '1.3.1' );
+define( 'LENDSURE_VERSION', '1.4.0' );
 define( 'LENDSURE_FILE', __FILE__ );
 define( 'LENDSURE_DIR', plugin_dir_path( __FILE__ ) );
 define( 'LENDSURE_URL', plugin_dir_url( __FILE__ ) );
@@ -24,6 +24,8 @@ define( 'LENDSURE_URL', plugin_dir_url( __FILE__ ) );
 require_once LENDSURE_DIR . 'includes/class-lendsure-db.php';
 require_once LENDSURE_DIR . 'includes/class-lendsure-calculator.php';
 require_once LENDSURE_DIR . 'includes/class-lendsure-reminders.php';
+require_once LENDSURE_DIR . 'includes/class-lendsure-reports.php';
+require_once LENDSURE_DIR . 'includes/class-lendsure-management.php';
 require_once LENDSURE_DIR . 'includes/class-lendsure-admin.php';
 
 register_activation_hook( __FILE__, array( 'LendSure_DB', 'activate' ) );
@@ -34,6 +36,8 @@ function lendsure_boot() {
     LendSure_DB::maybe_upgrade();
     LendSure_Reminders::instance();
     LendSure_Reminders::activate();
+    LendSure_Reports::instance();
+    LendSure_Management::instance();
     LendSure_Admin::instance();
 }
 add_action( 'plugins_loaded', 'lendsure_boot' );
